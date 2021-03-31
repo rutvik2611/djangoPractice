ef4b74c9259d9894672497381fac5af6cc3248b8
rutvik2611 <rutvik2611@gmail.com>
2021-03-31 00:45:01 -0400

    Created a Person Model

--- FIRSTPROJECT/FIRSTAPPLICATION/models.py
+++ FIRSTPROJECT/FIRSTAPPLICATION/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    First_name = models.CharField(max_length=30)
+    Last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    psw = models.CharField(max_length=30)
+    
