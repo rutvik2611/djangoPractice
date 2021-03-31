a26a8c53d0b50460bf5bf021376944e9b053cc9e
rutvik2611 <rutvik2611@gmail.com>
2021-03-31 13:43:33 -0400

    Forms(made a basic Templet)

--- FIRSTPROJECT/FIRSTAPPLICATION/forms.py
+++ FIRSTPROJECT/FIRSTAPPLICATION/forms.py
@@ -1,1 +1,7 @@
-from django import form
+from django import forms
+
+class Signupforms(forms.form):
+    First_name=forms.CharField(label="First Name :")
+    Last_name=forms.CharField(label="Last Name :")
+    Email=foms.EmailFiels(max_length=40)
+    psw=forms.CharField(widget=forms.PasswordInput)
