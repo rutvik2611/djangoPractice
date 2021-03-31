93791c9cb14c66daf09ae3e45ddcbed9cd08f089
rutvik2611 <rutvik2611@gmail.com>
2021-03-31 13:36:25 -0400

    Registered customer to admin

--- FIRSTPROJECT/FIRSTAPPLICATION/admin.py
+++ FIRSTPROJECT/FIRSTAPPLICATION/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from FIRSTAPPLICATION.models import Person,Students,Training
+from FIRSTAPPLICATION.models import Person,Students,Training,ContactInfor,Customer
 
 # Register your models here.
 
 admin.site.register(Person)
 admin.site.register(Students)
 admin.site.register(Training)
+#admin.site.register(ContactInfor)
+admin.site.register(Customer)
