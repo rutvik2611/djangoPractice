281c6c13009c6458c254e3317e9c1b6c3f794d64
rutvik2611 <rutvik2611@gmail.com>
2021-03-31 01:31:57 -0400

    Added Traning and Students Models

--- FIRSTPROJECT/FIRSTAPPLICATION/models.py
+++ FIRSTPROJECT/FIRSTAPPLICATION/models.py
@@ -6,4 +6,17 @@ class Person(models.Model):
     Last_name = models.CharField(max_length=30)
     email = models.CharField(max_length=30)
     psw = models.CharField(max_length=30)
+
+
+class Training(models.Model):
+    session = models.CharField(max_length=5,unique=True)
+    def __str__(slef):
+        return self.session
+
+class Students(models.Model):
+    name = models.CharField(max_length=30)
+    session=models.ForeignKey("Training", on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
     
