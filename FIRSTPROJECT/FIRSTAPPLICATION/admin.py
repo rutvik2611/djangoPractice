215c1df2d19d5bbf1c47be7de42978bbfd476b8e
rutvik2611 <rutvik2611@gmail.com>
2021-03-31 01:33:23 -0400

    Register them to admin

--- FIRSTPROJECT/FIRSTAPPLICATION/admin.py
+++ FIRSTPROJECT/FIRSTAPPLICATION/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from FIRSTAPPLICATION.models import Person
+from FIRSTAPPLICATION.models import Person,Students,Training
 
 # Register your models here.
 
 admin.site.register(Person)
+admin.site.register(Students)
+admin.site.register(Training)
