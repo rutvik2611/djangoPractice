5bba46ed9b4fcd093c5f0dc7708b44dc2303fd0b
rutvik2611 <rutvik2611@gmail.com>
2021-03-31 13:09:23 -0400

    Fix Bugs

--- FIRSTPROJECT/FIRSTAPPLICATION/models.py
+++ FIRSTPROJECT/FIRSTAPPLICATION/models.py
@@ -10,7 +10,7 @@ class Person(models.Model):
 
 class Training(models.Model):
     session = models.CharField(max_length=5,unique=True)
-    def __str__(slef):
+    def __str__(self):
         return self.session
 
 class Students(models.Model):
