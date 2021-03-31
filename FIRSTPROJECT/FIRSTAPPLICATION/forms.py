78c1d8edca67d22922901ff686ef73c31c242b56
rutvik2611 <rutvik2611@gmail.com>
2021-03-31 14:32:57 -0400

    Madde forms

--- FIRSTPROJECT/FIRSTAPPLICATION/forms.py
+++ FIRSTPROJECT/FIRSTAPPLICATION/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
-class Signupforms(forms.form):
+class Signupforms(forms.Form):
     First_name=forms.CharField(label="First Name :")
     Last_name=forms.CharField(label="Last Name :")
-    Email=foms.EmailFiels(max_length=40)
+    Email=forms.EmailField(max_length=40)
     psw=forms.CharField(widget=forms.PasswordInput)
