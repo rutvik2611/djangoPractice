2ad6db0f728aef0aa773e0a0eeb4598a257be146
rutvik2611 <rutvik2611@gmail.com>
2021-03-31 15:07:10 -0400

    update urls

--- FIRSTPROJECT/FIRSTAPPLICATION/urls.py
+++ FIRSTPROJECT/FIRSTAPPLICATION/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path,include
-from FIRSTAPPLICATION import views
+from FIRSTAPPLICATION import views,forms
 urlpatterns = [
     
     path('',views.not_home),
     path('r/',views.home),
-    path('signup/',views.signup)
+    path('signup/',views.signup),
+    path('register/',views.signup2)
     
     
 ]
