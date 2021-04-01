c7d0a8faf783c340f7b1aea77729d37c661b63cf
rutvik2611 <rutvik2611@gmail.com>
2021-04-01 19:48:42 -0400

    Make Models(One with inheritance)

--- FIRSTPROJECT/SECONDAPPLICATION/models.py
+++ FIRSTPROJECT/SECONDAPPLICATION/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+def register(models.Model):
+    First_name=models.CharField(max_length=40)
+    Last_name=models.CharField(max_length=40)
+    Email=models.EmailField(max_length=40)
+    Password=models.CharField(max_length=40)
+    
+    def __str__(self):
+        return self.First_name
+
+def register_blog(register):
+    BlogName=models.CharField(max_length=40)
+
+     def __str__(self):
+        return self.BlogName
