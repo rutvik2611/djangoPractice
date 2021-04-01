a746d3b2baa9f616e5f3704bcbbcbfae65158758
rutvik2611 <rutvik2611@gmail.com>
2021-04-01 11:38:31 -0400

    validation from django core validators

--- FIRSTPROJECT/FIRSTAPPLICATION/forms.py
+++ FIRSTPROJECT/FIRSTAPPLICATION/forms.py
@@ -1,7 +1,8 @@
 from django import forms
+from django.core import validators
 
 class Signupforms(forms.Form):
     First_name=forms.CharField(label="First Name :")
     Last_name=forms.CharField(label="Last Name :")
     Email=forms.EmailField(max_length=40)
-    psw=forms.CharField(widget=forms.PasswordInput)
+    psw=forms.CharField(widget=forms.PasswordInput, validators=[validators.MinLengthValidator(2)])
