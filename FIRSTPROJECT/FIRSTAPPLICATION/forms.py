248f7e88ff325ecaaaeef2034e15449c1106d5b5
rutvik2611 <rutvik2611@gmail.com>
2021-04-01 12:06:15 -0400

    fix

--- FIRSTPROJECT/FIRSTAPPLICATION/forms.py
+++ FIRSTPROJECT/FIRSTAPPLICATION/forms.py
@@ -1,8 +1,16 @@
 from django import forms
 from django.core import validators
 
+def check(value):
+    #print(value)
+    if len(value)<6:
+        #print(type(value))
+        raise forms.ValidationError("Should not be greater than 5")
+       
+            
+
 class Signupforms(forms.Form):
     First_name=forms.CharField(label="First Name :")
     Last_name=forms.CharField(label="Last Name :")
     Email=forms.EmailField(max_length=40)
-    psw=forms.CharField(widget=forms.PasswordInput, validators=[validators.MinLengthValidator(2)])
+    psw=forms.CharField(widget=forms.PasswordInput, validators=[check])
