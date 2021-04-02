c279ca68519771453093f8c2c11e0a918475c873
rutvik2611 <rutvik2611@gmail.com>
2021-04-02 15:39:06 -0400

    ALl

---
+++ FIRSTPROJECT/BLOG/forms.py
@@ -0,0 +1,11 @@
+from django import froms
+from django.forms import ModelForm
+from .models import LOGIN
+
+class LOGIN(ModelForm):
+
+    class Meta:
+        model = LOGIN
+        fields = "__all__"
+
+    
