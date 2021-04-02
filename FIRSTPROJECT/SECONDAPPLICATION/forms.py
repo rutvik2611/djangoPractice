c047db885f85b15a010a9f82137db23359119928
rutvik2611 <rutvik2611@gmail.com>
2021-04-02 13:27:52 -0400

    Make a form for that model

--- FIRSTPROJECT/SECONDAPPLICATION/forms.py
+++ FIRSTPROJECT/SECONDAPPLICATION/forms.py
@@ -1,8 +1,13 @@
 from django.forms import ModelForm
-from .models import register
+from .models import register,Employee
 
 class registration_form(ModelForm):
     
     class Meta:
         model = register
+        fields = "__all__"
+
+class EmployeeForm(ModelForm):
+    class Meta:
+        model = Employee
         fields = "__all__"
