3bce8f9e98e99a71543a5f613e70b7c2f065ab1a
rutvik2611 <rutvik2611@gmail.com>
2021-04-01 23:01:52 -0400

    render html

--- FIRSTPROJECT/SECONDAPPLICATION/views.py
+++ FIRSTPROJECT/SECONDAPPLICATION/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from . import forms
 # Create your views here.
+# def home(request):
+
+#     return HttpResponse("Hello Home")
+
 def home(request):
-    return HttpResponse("Hello Home")
+    passing_form = forms.registration_form() #If i comment this out i get an error passing_form refered before intiating
+    if request.method == "POST":
+        passing_form = forms.registration_form(request.POST)
+        if passing_form.is_valid():
+            try:
+                passing_form.save()
+            except:
+                print("Errorrr")
+        else:
+            passing_form = forms.registration_form() 
+                       
+    return render(request,'SECONDAPPLICATION/Registration.html',{'form':passing_form})
