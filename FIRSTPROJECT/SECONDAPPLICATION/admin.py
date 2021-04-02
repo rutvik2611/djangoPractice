758b36ad6de58e024f207f1b8e583bc06604c7d4
rutvik2611 <rutvik2611@gmail.com>
2021-04-01 21:52:51 -0400

    Register Models

--- FIRSTPROJECT/SECONDAPPLICATION/admin.py
+++ FIRSTPROJECT/SECONDAPPLICATION/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from SECONDAPPLICATION.models import register_blog,register
 # Register your models here.
+
+
+admin.site.register(register)
+admin.site.register(register_blog)
