64fc30a60ec7a0e175f1d95962a68d5430c6a68e
rutvik2611 <rutvik2611@gmail.com>
2021-04-03 17:19:31 -0400

    Model:UniqueConstraint

--- FIRSTPROJECT/BLOG/models.py
+++ FIRSTPROJECT/BLOG/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.core.validators import RegexValidator
-
+from django.contrib.auth.models import User
 # Create your models here.
-class LOGIN(models.Model):
-    #phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-    username = models.CharField(max_length=40)
-    user_password = models.CharField(max_length=40)
-    email =models.EmailField()
-    #phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+class Regsistration(models.Model):
+    
+    username = models.CharField(max_length=40,unique=True)
+    password = models.CharField(max_length=40)
+    email =models.EmailField(unique = True)
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+    
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    #password_regex = RegexValidator(regex='^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$')
+    def __str__(self):
+        return self.username
